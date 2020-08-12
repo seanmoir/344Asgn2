d97d25c7207a4f5ab137a6805c2f80b18a259543
bbosecke <brodie.bosecke.yt@gmail.com>
2020-08-12 12:00:43 +1200

    OUTLINE for ASGN2

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # 344Asgn2
 Assignment 2 for COSC344
+
+TO DO:
+
+Relational Schema (no specified software to use it)
+
+Normalization (follow lecture 9 to do the 1NF, 2NF... etc...
+
+load.sql (format is already there, need to add values, keys, constraints etc...
---
+++ load.sql
@@ -0,0 +1,25 @@
+DROP TABLE customer;
+DROP TABLE order;
+DROP TABLE employee;
+DROP TABLE payment;
+DROP TABLE delivery;
+DROP TABLE product;
+DROP TABLE category;
+
+CREATE TABLE customer();
+
+CREATE TABLE order();
+
+CREATE TABLE employee();
+
+CREATE TABLE payment();
+
+CREATE TABLE delivery();
+
+CREATE TABLE product();
+
+CREATE TABLE category();
+
+INSERT INTO employee VALUES();
+
+COMMIT;
